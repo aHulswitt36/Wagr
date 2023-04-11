fafee976a5b8b4fd9b93811a766c1093c596999c
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2023-04-11 06:06:49 -0400

    deleted a lot of the WEb3Auth stuff as we won't be using it.  Adding in a BaseApiConnector and created an Extensions class

--- src/Wagr/Wagr.ClientInfrastructure/BaseApiConnector.cs
+++ src/Wagr/Wagr.ClientInfrastructure/BaseApiConnector.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Wagr.ClientInfrastructure.Extensions;
 
 namespace Wagr.ClientInfrastructure
 {
     internal abstract class BaseApiConnector
     {
         private readonly HttpClient _httpClient;
+        private string _endpointUrl;
 
         public abstract string Controller { get; }
 
         public BaseApiConnector(HttpClient httpClient) 
         {
             _httpClient = httpClient;
+            _endpointUrl = Controller + "/";
         }
 
-        public async Task<T> PostAsync()
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest request, CancellationToken token)
+        {
+            var endpointUrl = _endpointUrl + endpoint;
+            var response = await _httpClient.PostAsJsonAsync(endpointUrl, request, token);
+            await response.CheckForSuccessfulResponse();
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        
     }
 }
--- src/Wagr/Wagr/Pages/Account/Login/Login.js
+++
@@ -1,6 +0,0 @@
-export {};
-//export async function CreateWeb3AuthConnection() {
-//    var auth = new web3auth();
-//    return await auth.connect();
-//}
-//# sourceMappingURL=Login.js.map
